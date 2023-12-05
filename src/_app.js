33b7be9d661fd9a99277230232f06a40064b2d67
Giovana Birck <gmattosbirck@my.bcit.ca>
2023-12-05 12:21:40 -0800

    first commit

---
+++ src/_app.js
@@ -0,0 +1,12 @@
+import Todos from "./components/Todos";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>Task management app</h2>
+      <Todos />
+    </div>
+  );
+}
